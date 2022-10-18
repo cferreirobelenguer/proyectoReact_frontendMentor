0dd6f9e7496bfa3e54f262e217f392dc6fcfc7bf
Carolina <cferrebe@emeal.nttdata.com>
2022-10-18 19:17:53 +0200

    container 2 desktop

--- src/components/Container2.jsx
+++ src/components/Container2.jsx
@@ -3,7 +3,24 @@ import React from "react"
 const Container2=()=>{
     return(
         <div class="container2">
-
+            <div class="container2_title_graphic">
+                <h2>Graphic Design</h2>
+            </div>
+            <div class="container2_title_ui">
+                <h2>UI/UX</h2>
+            </div>
+            <div class="container2_title_apps">
+                <h2>Apps</h2>
+            </div>
+            <div class="container2_title_photo">
+                <h2>Photography</h2>
+            </div>
+            <div class="container2_title_illustrations">
+                <h2>Illustrations</h2>
+            </div>
+            <div class="container2_title_motions">
+                <h2>Motions Graphics</h2>
+                </div>
         </div>
     )
 }
